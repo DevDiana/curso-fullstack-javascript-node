1c4d104cf3a725b226a31f0cae8b98db2b74c610
Diana Regina da Silva <M269396>
2023-05-16 23:28:07 -0300

    mais um dia de estudos

---
+++ aula14/index.js
@@ -0,0 +1,15 @@
+// window.document.body.innerHTML+='qualquer coisa <br />';
+// window.document.body.innerHTML+='qualquer coisa <br />';
+// window.document.body.innerHTML+='qualquer coisa <br />';
+// const nome = prompt('Digite seu nome: ');
+// const idade = prompt('Digite sua idade: ');
+
+// document.body.innerHTML = `O nome é: <strong>${nome}</strong> e a sua idade: <strong>${idade}</strong>`
+
+
+let num1 = 1;
+let num2 = 2.5;
+
+console.log(num1.toString() + num2);
+console.log(Number.isInteger(num1));
+console.log(Number.isNaN(num1))
